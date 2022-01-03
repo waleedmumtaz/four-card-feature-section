b045eed194fc831a1b8cc577a8c18960f66312ae
Waleed <waleedthedev@gmail.com>
2022-01-03 16:00:51 +0500

    Setup project according to the style guides

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
 <script>
 </script>
 
-<main>
-</main>
+<svelte:head>
+  <title>Four Card Feature Section</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin="anonymous"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+<main>Content here</main>
 
 <style>
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,30 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            red: 'hsl(0, 78%, 62%)',
+            cyan: 'hsl(180, 62%, 55%)',
+            orange: 'hsl(34, 97%, 64%)',
+            blue: 'hsl(212, 86%, 64%)',
+          },
+          neutral: {
+            'dark-blue': 'hsl(234, 12%, 34%)',
+            'grayish-blue': 'hsl(229, 6%, 66%)',
+            'light-gray': 'hsl(0, 0%, 98%)',
+          },
+        },
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
